0bd624c910b4b0ae58d691c8feb4532993570f24
zhudi2035 <348084312@qq.com>
2022-05-18 17:00:22 +0800

    first

--- src/main/java/com/xindi/controller/GraceExceptionHandler.java
+++ src/main/java/com/xindi/controller/GraceExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.xindi.controller;
 
+import com.xindi.utils.JSONResult;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/*
+同意异常拦截处理
+ */
+@ControllerAdvice
 public class GraceExceptionHandler {
+
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    @ResponseBody
+    public JSONResult returnMaxFileSizeLimit(FileSizeLimitExceededException e) {
+        return JSONResult.errorMsg("文件大小不能超过500KB");
+    }
 }
